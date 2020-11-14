6eb191040dabd26dc4ecc1ce198d6d9a0fcf68b5
Andrei Ciocodeica <andrei.ciocodeica.ac@gmail.com>
2020-11-14 11:56:56 +0200

    Work in progress

---
+++ 16MovieTicketsApp(in lucru)/app.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = Number(movieSelect.value);
+
+
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    count.innerHTML = selectedSeats.length; 
+    total.innerHTML = selectedSeats.length * ticketPrice;
+}
+
+movieSelect.addEventListener('change', function(e){
+    ticketPrice = Number(e.target.value);
+    // console.log(ticketPrice);
+    updateSelectedCount();
+});
+
+container.addEventListener("click", function(e){
+    // console.log(e.target);
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected'); //la clasele neocupate dand click i se modifica clasa
+    }
+    updateSelectedCount();
+});
+//e este evenimentul de click
+//contains()
+//toogle()
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # MyApps-Webs
+
+## Small landing pages and apps.
+
+### This projects are created with VanillaJS/HTML/CSS
+
+<div>
+    <h4>What projects you can find:</h4>
+    <ol>
+        <li>ContactFormWeb</li>
+        <li>BookListApp</li>
+        <li>CountdownTimerApp</li>
+        <li>QuizApp</li>
+        <li>MovieApp</li>
+        <li>NotesApp</li>
+        <li>ToDoApp</li>
+        <li>RecipeApp</li>
+        <li>GitHubProfilesApp</li>
+        <li>DrawingApp</li>
+        <li>PasswordGeneratorApp</li>
+        <li>WeatherApp</li>
+        <li>CocktailPickerApp(WorkinProgress)</li>
+        <li>RestaurantWeb</li>
+        <li>WeatherMapWeb</li>
+        <li>MovieTicketsApp(WorkinProgress)</li>
+    </ol>
+</div>
+
+<h4>Visuals</h4>
+<img src="https://images.unsplash.com/photo-1569396116180-210c182bedb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"/>
+
+<p>Project status: <span style="font-weight: bold;">Work in progress<span></p>
